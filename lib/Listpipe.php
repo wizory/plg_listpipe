f879644e1a2f869f766b5e5a58e3581b2ddea347
Josh Guice <jguice@gmail.com>
2016-05-30 10:24:26 -0700

    feat(tests): initial structure

--- lib/Listpipe.php
+++ lib/Listpipe.php
@@ -4,8 +4,26 @@ namespace Wizory;
 
 class Listpipe {
 
-    public function test()
-    {
-        return 'foo';
+    const FAIL = 'fail';
+    const OK = 'success';
+
+    protected $is_draft;
+
+    public function __construct() {
+        $this->is_draft = False;
     }
+
+    public function get_draft($draft_key, $approval_key, $blog_posting_id, $approve_type='', $debug=False) {
+        // require all args to be set to non-empty values
+        if (empty($draft_key) || empty($approval_key) || empty($blog_posting_id)) { return Listpipe::FAIL; }
+
+        if ($approve_type == 'draft') {
+            $this->is_draft = True;
+        }
+    }
+
+    public function is_draft() {
+        return $this->is_draft;
+    }
+
 }
--- tests/ListpipeTest.php
+++ tests/ListpipeTest.php
@@ -3,15 +3,47 @@
 use Wizory\Listpipe;
 
 class ListpipeTest extends PHPUnit_Framework_TestCase {
-    public function testNothing() {
 
-        // is an empty string empty?  does it make the sound of one hand clapping?
+    protected $listpipe;
+
+    protected function setUp() {
+        # create a new listpipe object before every test
+        $this->listpipe = new Listpipe();
+    }
+    
+    public function testNothing() {
+        # is an empty string empty?  does it make the sound of one hand clapping?
         $this->assertEmpty('');
     }
 
-    public function testClassInstantiation() {
-        $listpipe = new Listpipe();
+    public function testEmptyGetDraftArgs() {
+        $result = $this->listpipe->get_draft('', '', '');
+        $this->assertEquals('fail', $result);
 
-        $this->assertEquals($listpipe->test(), 'foo');
+        $result = $this->listpipe->get_draft('asdf', '', '');
+        $this->assertEquals('fail', $result);
+
+        $result = $this->listpipe->get_draft('', 'asdf', '');
+        $this->assertEquals('fail', $result);
+
+        $result = $this->listpipe->get_draft('', '', 'asdf');
+        $this->assertEquals('fail', $result);
     }
+
+    public function testGetDraftDefaultApproveType() {
+        $this->listpipe->get_draft('foo', 'bar', 'baz');
+
+        $this->assertFalse($this->listpipe->is_draft());
+    }
+
+    public function testGetDraftExplicitApproveType() {
+        $this->listpipe->get_draft('foo', 'bar', 'baz', 'draft');
+
+        $this->assertTrue($this->listpipe->is_draft());
+    }
+
+    # TODO add tests for debug mode
+
+
+
 }
