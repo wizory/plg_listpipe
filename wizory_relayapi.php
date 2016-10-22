f4f10e3cb91f286bc9c7b27a2b5aba6a87ba4f75
Josh Guice <jguice@gmail.com>
2016-10-22 11:07:57 -0700

    fix(interval): use unix seconds to compute
    - DateTime::diff doesn't convert all interval elements to a single unit (e.g. days -> hours)
    - cleanup locked code, rearrange

--- wizory_relayapi.php
+++ wizory_relayapi.php
@@ -25,37 +25,36 @@ class plgSystemWizory_Relayapi extends \JPlugin {
     // NOTE this needs to remain very lightweight since it's called on *every* request
     public function onAfterRender() {
 
-        # TODO determine a way to lock so only one simultaneous request is allowed to actually pull articles...
-        # alternatively just check for articles by title before creating to avoid dupes
-
         # load our plugin's extension table data
         $table = new JTableExtension(JFactory::getDbo());
         $table->load(array('element' => 'wizory_relayapi'));
+
+        $checked_out_time = $table->get('checked_out_time');
+
+        # lock to prevent more than one simultaneous relayapi update process
+        # using checked_out_time because Joomla 3.6.2 isn't setting checked_out :(
+        if (empty($checked_out_time) || $checked_out_time == '0000-00-00 00:00:00') {  # not locked
+            $table->checkOut($this->params->user_id);
+        } else {  # locked
+            return;
+        }
+
+        $now = new Datetime();
+
+        $config = json_decode($this->params['params']);
 
         $custom_data = json_decode($table->custom_data, true);  # true yields an associative array vs. object
 
         if (! isset($custom_data)) { $custom_data = array(); }
 
-        # lock to prevent more than one simultaneous relayapi update process
-        if ($table->get('checked_out_time') != '0000-00-00 00:00:00') {  # Joomla 3.6.2 isn't setting checked_out :(
-            return;
-        } else {
-            $table->checkOut($this->params->user_id);
-        }
-
-        $config = json_decode($this->params['params']);
-
-        $now = new Datetime();
-
         if (isset($custom_data['last_updated']['date'])) {
             $last_updated = DateTime::createFromFormat('Y-m-d H:i:s.000000', $custom_data['last_updated']['date']);
 
-            $elapsed = $now->diff($last_updated);
+            $elapsed = ($now->getTimestamp() - $last_updated->getTimestamp()) / 3600;  # convert seconds to hours
 
             # if we haven't surpassed the update interval, bail
-            if ($elapsed->h < $config->update_interval) {
+            if ($elapsed < $config->update_interval) {
                 $table->checkIn();
-
                 return;
             }
         }
